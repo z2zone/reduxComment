e9fed975a6178c5c99d85cd33d1083e23f1229e6
Brandon Cho <hjun1114@gmail.com>
2023-03-01 20:32:32 -0500

    adding axios and redux-thunk config

--- comment/src/actions/index.js
+++ comment/src/actions/index.js
@@ -1,5 +1,8 @@
+import { jsonPlaceHolder } from "../apis/jsonPlaceHolder";
+
 export const fetchPost = () => {
-    return {
-        type: 'FETCH_POST'
-    };
+    return async (dispatch) => {
+        const response = await jsonPlaceHolder.get('/posts');
+        dispatch({type:'FETCH_POST', payload: response});
+    }
 };
--- comment/src/index.js
+++ comment/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
 const rootElement = document.getElementById('root');
 const reactRoot = ReactDom.createRoot(rootElement);
+const store = createStore(reducers, applyMiddleware(thunk))
 
 reactRoot.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App/>
     </Provider>
 )
