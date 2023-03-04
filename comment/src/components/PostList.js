5f749b61970ac52b4c85b30556f30371d0a6c9ea
Brandon Cho <hjun1114@gmail.com>
2023-03-03 23:56:23 -0500

    adding style to the comment

--- comment/src/components/PostList.js
+++ comment/src/components/PostList.js
@@ -6,10 +6,24 @@ const PostList = (props) => {
     useEffect(()=>{
         props.fetchPost();
     }, []);
-
+    const rednerList = () => {
+        return props.posts.map(post => {
+           return (
+            <div className='item' key={post.id}>
+                <i className='large middle aligned icon user'/>
+                <div className='content'>
+                    <div className='description'>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </div>
+                </div>
+            </div>
+           ); 
+        });
+    }
     return(
-        <div>
-            PostList
+        <div className="ui relaxed divided list">
+            {rednerList}
         </div>
     );
 }
